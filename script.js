c218b20f9aeef3c3533f091d4e7333dca1432cf1
Asmi1804 <162876660+Asmi1804@users.noreply.github.com>
2025-07-04 21:11:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Placeholder for future interactivity
+console.log("Portfolio Loaded");
+  function showThankYou() {
+    // Delay showing success message
+    setTimeout(() => {
+      document.getElementById('formSuccess').style.display = 'block';
+    }, 2000);
+    return true; // allow form to submit
+  }
